7bb69ca5b4102fda8208daac000da38da5e77473
sameerjagdale <sameerjagdale90@yahoo.co.in>
2013-01-30 21:26:32 -0500

    Given python script  is used to automatically grade assignments by parsing output for specific keywords.

--- autograde.py
+++ autograde.py
@@ -1,51 +1,56 @@
 import subprocess
 import glob
+import datetime
 
+
+#start of executeFile . Function executes compiled file and return output
 def executeFile(fileName) :
         p=subprocess.Popen("./"+fileName.split(".")[0], shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-        #for lines in p.stdout.readlines() :
-        #        print lines
 	return p.stdout.readlines()
 #end of executeFile 
 
-	
+# start of extract name. Function extracts student name from the file name passed as an argument. 	
 def extractName(fileName):
 	return fileName.split("_")[0]
-	
-	
+#end of extractName 
+
+#start of writeToLog. Writes given message to log after appending date and time 
+def writeToLog(message):
+	fout=open("log.txt","a")
+	fout.write(str(datetime.datetime.now())+":"+message+ "\n")
+#end of writeToLog
+
+#start of hasError. Checks for error based on given keyword	
 def hasError(output,word):
 	for line in output:
 		if line.lower().find(word) >=0:
 			return True
 	return False
 #end of hasError
+
+#start of parseOutput.parses output for keywords present  in the file . 
 def parseOutput(output,keywordFile) :
 	try:
 		fin=open(keywordFile,"r")
 		count=0
-		#num=0
-		#num1=0
-		#print "number of lines read"+str(len(fin.readlines()))
 		for keyword in fin.readlines():
-			#num=num+1
-			#num1=0
-			#print keyword
+			
 			for line in output:
-				#num1=num1+1
+				
 				if line.lower().strip().find(keyword.lower().strip())>=0:
 					count=count+1
-					#print keyword
+					
 					break
-			#print "num1="+str(num1)
+			
 		fin.close()
-		#print "num="+str(num)
-		#print "1st count="+str(count)
+		
 	except IOError:
 		print "keyword file not found"
 		return 0
 	return count	
-			
-	
+#end of parseOutput		
+
+#start of assignScore. assigns scores on the basis of number of keywords found. 
 def assignScore(numKeys):
 	if numKeys==0:
 		return 80
@@ -55,10 +60,14 @@ def assignScore(numKeys):
 		return 95
 	else:
 		return 100
+#end of assignscore
+		
+#writes score to file.
 def writeToFile(student, score):
 	fout=open("score.csv","a")
 	fout.write(student+","+str(score)+"\n")
 	
+#end of writeToFile
 #start of script
 i=1
 listOfFiles=list()
@@ -69,14 +78,14 @@ for fileName  in listOfFiles:
 	p=subprocess.Popen("gcc -o"+fileName.split('.')[0]+" "+fileName, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 	output=p.stdout.readlines()	
 	if not hasError(output,"error"):
-		print "Successful compilation for "+ fileName 
-		print "output: "
+	
+		writeToLog("Successful compilation for "+ fileName) 
 		output=executeFile(fileName)
 		numKeys= parseOutput(output,"keyword.txt")
-		print "number of keys found "+str(numKeys)
+		writeToLog("in file " +fileName+" number of keywords found = "+str(numKeys))
 		
 	else :
-		print "error in compilation for:"+fileName
+		writeToLog("error in compilation for:"+fileName)
 	name=extractName(fileName)
 	score=assignScore(numKeys)
 	writeToFile(name,score)
